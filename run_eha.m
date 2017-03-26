374a54f352560a0514b1ce5ab02e99a1adf11a65
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2017-03-25 23:46:36 -0400

    Added EHA model along with the m file that runs it. Currently have the transfer function along with the gain.

---
+++ run_eha.m
@@ -0,0 +1,29 @@
+Kc = 1; %Nm/A
+Kw = 0.6; %V/rad/s
+%%Known variables
+Rc = 0.5; %ohm
+Lc = 0.004; %H
+Jpm = 1.6e-3; %Kgm^2
+M = 20; %Kg
+Dp = 1.7e-7; %m^3/rad
+Ka1 = 6980; %rad/s/m
+Ka2 = 41.87; %rad/s/V
+Kp = 1.2*Ka2;
+Ki = 2.4*Ka2;
+Kpos = 1*Ka1;
+Vo = 6.85e-5; %m^3
+A = 5.05e-4; %m^2
+Psens = 1e-6; %m
+Vsens = 1; %rad/s
+Ma = 8; %Kg
+x_max = 12; %cm
+Be = 2.1e8; %Pa
+Kf = 2.4e-4; %Nm/rad/s
+B = 760; %N/m/s
+L2ep = 5e-13; %m^3/s/Nm^-2
+Kpipe = 2.5e12; %Pa/m^6/s^2
+
+%Linear EHA Model Block Diagram variables
+x_d = 10; %%desired x
+
+

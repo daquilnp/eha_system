5b9877f345e816bb50e738060391ac42db4c4893
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2017-03-29 16:30:55 -0400

    Updated pole location and took screenshot of observer response being within specs (Ts = 0.1 and overshoot of 4%)

--- run_eha.m
+++ run_eha.m
@@ -2,17 +2,16 @@ NG = 13.2;
 DG = [1 40 39150 0];
 G = tf(NG,DG);
 [A,B,C,~] = tf2ss(NG,DG);
-D = [0;0;0];
+D = 0;
 z = 0.707;
 Ts = 0.1;
 wn = 4/(z*Ts);
 r = roots([1,2*z*wn, wn^2]);
-poles =  [r' -40];
+poles =  [r' -150];
 K = place(A,B,poles);
-X_O=[0.001;0;0];
-L = place(A', C', 10*poles)
+L = place(A', C', 10*poles);
 L = L';
-sys=ss(A,B,C,0);
+sys=ss(A,B,C,D);
 Nbar= rscale(sys,K);
-open('observer_test_sim.slx');
+open('eha_model.slx');
 

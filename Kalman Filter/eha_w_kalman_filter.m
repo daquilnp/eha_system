8ab3fb53873fb8c533996dc35cd6293f6cf785a6
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2017-04-12 09:29:28 -0400

    added kaman filter so far. something wrong with calculation.

--- Kalman Filter/eha_w_kalman_filter.m
+++ Kalman Filter/eha_w_kalman_filter.m
@@ -1,26 +1,29 @@
 %% EHA system with Kalman Filter
-
-A = [ 1 0.001 0; 0 1 0.001; -557.02 -28616 0.9418];
-B = [0 0 557.02];
+A = [ 1 0.001 0; 0 1 0.001; -557.02 -28.616 0.9418];
+B = [0 0 557.02]';
 H = [1 0 0];
-w_max = [0.01 1 10];
+C = H;
+D = 0;
+zeta = 0.707;
+Ts = 0.1;
+wn = 4/(zeta*Ts);
+r = roots([1,2*zeta*wn, wn^2]);
+poles =  [r' -150];
+K = place(A,B,poles);
+sys=ss(A,B,H,D);
+Nbar= rscale(sys,K);
+
+
+w_max = [0.01 1 10]';
 V_max = 0.1;
 sample_t = 1e-3;
 Q = [ 1 0 0; 0 10 0; 0 0 100];
-R = [0.1 0 0 ; 0 100 0; 0 0 1000];
-x = [0 0 0]'; %intial x
-P = 10*Q; %intial P
-u = 1;
-z = H*x;
-
-   %%Begin filter
-   x = A*x + B*u;
-   P = A * P * A' + Q;
+R = 10;
+x0 = [0 0 0]'; %intial x
+P0 = 10*Q; %intial P
 
 
-   % Compute Kalman gain factor:
-   K = P*H'*inv(H*P*H'+ R);
 
-   % Correction based on observation:
-   x = x + K*(z-H*x);
-   P = P - K*H*P;
+
+
+
---
+++ Kalman Filter/run_eha.m
@@ -0,0 +1,16 @@
+NG = 13.2;
+DG = [1 40 39150 0];
+G = tf(NG,DG);
+[A,B,C,D] = tf2ss(NG,DG);
+z = 0.707;
+Ts = 0.1;
+wn = 4/(z*Ts);
+r = roots([1,2*z*wn, wn^2]);
+poles =  [r' -150];
+K = place(A,B,poles);
+L = place(A', C', 10*poles);
+L = L';
+sys=ss(A,B,C,D);
+Nbar= rscale(sys,K);
+open('eha_model_w_smc.slx');
+

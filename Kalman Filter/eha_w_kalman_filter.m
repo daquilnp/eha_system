98999c5c387876c4f1e0a8e890dccada538ef802
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2017-04-15 12:11:49 -0400

    Added the SVSF filter!

--- Kalman Filter/eha_w_kalman_filter.m
+++ Kalman Filter/eha_w_kalman_filter.m
@@ -4,23 +4,11 @@ B = [0 0 557.02]';
 H = [1 0 0];
 C = H;
 D = 0;
-zeta = 0.707;
-Ts = 0.1;
-wn = 4/(zeta*Ts);
-r = roots([1,2*zeta*wn, wn^2]);
-poles =  [r' -150];
-K = place(A,B,poles);
-sys=ss(A,B,H,D);
-Nbar= rscale(sys,K);
 
 
 w_max = [0.01 1 10]';
 V_max = 0.1;
-sample_t = 1e-3;
-Q = 0.001*[ 1 0 0; 0 10 0; 0 0 100];
-R = 10;
 x0 = [0 0 0]'; %intial x
-P0 = 10*Q; %intial P
 u = 0;
 
 
